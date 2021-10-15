451b645ecd5cb4cc43688ed1b8c33d716b76213a
anna1444 <annatsurkan.23@gmail.com>
2021-10-15 11:25:48 +0300

    Added hibernate

--- src/main/java/com/ods/learning/Main.java
+++ src/main/java/com/ods/learning/Main.java
@@ -1,6 +1,13 @@
 package com.ods.learning;
 
 import com.ods.learning.model.Car;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,5 +17,18 @@ public class Main {
         Car car = (Car) applicationContext.getBean("simpleCar");
         System.out.println(car);
         System.out.println();
+
+        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+
+        SessionFactory factory = meta.getSessionFactoryBuilder().build();
+        Session session = factory.openSession();
+        Transaction t = session.beginTransaction();
+
+        session.save(car);
+        t.commit();
+        System.out.println("successfully saved");
+        factory.close();
+        session.close();
     }
 }
--- src/main/java/com/ods/learning/model/Car.java
+++ src/main/java/com/ods/learning/model/Car.java
@@ -1,6 +1,14 @@
 package com.ods.learning.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String model;
     private String number;
